83888a8ac4136f79ededd0c8b6c6452b17e2c743
Manikandan Dhanapal <manikandanthedeveloper@gmail.com>
2025-11-19 13:39:10 +0530

    Login and Registration implemented

--- dashboard/src/views/auth/Login.tsx
+++ dashboard/src/views/auth/Login.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { FaFacebook, FaGoogle } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
-const Login = () => {
+const Register = () => {
     return (
-        <div>Login</div>
+        <div className='min-w-screen min-h-screen bg-[#ecebff] flex justify-center items-center overflow-hidden'>
+            <div className="md:w-[500px] sm:[350px] bg-blue-500 text-white sm:2 md:p-4">
+                <h2 className="text-xl mb-2 font-bold">Welcome to Ecommerce</h2>
+                <p className="text-sm mb-3 font-medium">Please Sign In your account</p>
+
+                <form>
+                    <div className='flex flex-col w-full gap-1 mb-3'>
+                        <label htmlFor="email">Email</label>
+                        <input className='px-3 py-2 outline-none border border-blue-400 bg-transparent rounded-md' type="email" name='email' placeholder='Email' id='email' required />
+
+                    </div>
+
+                    <div className='flex flex-col w-full gap-1 mb-3'>
+                        <label htmlFor="password">Password</label>
+                        <input className='px-3 py-2 outline-none border border-blue-400 bg-transparent rounded-md' type="password" name='password' placeholder='Password' id='password' required />
+                    </div>
+
+                    <button className='bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Sign In</button>
+
+                    <div className='flex items-center mb-3 gap-3 justify-center'>
+                        <p>Don't Have an account ? <Link className='font-bold' to="/register">Sing Up</Link> </p>
+                    </div>
+
+                    <div className='w-full flex justify-center items-center mb-3'>
+                        <div className='w-[45%] bg-amber-50 h-px'></div>
+                        <div className='w-[10%] flex justify-center items-center'>
+                            <span className='pb-1'>Or</span>
+                        </div>
+                        <div className='w-[45%] bg-amber-50 h-px'></div>
+                    </div>
+
+                    <div className='flex justify-center items-center gap-3'>
+                        <div className='w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+                            <span><FaGoogle /></span>
+                        </div>
+
+                        <div className='w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg hover:shadow-blue-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+                            <span><FaFacebook /></span>
+                        </div>
+
+                    </div>
+
+
+                </form>
+            </div>
+        </div>
     )
 }
 
-export default Login
+export default Register
--- dashboard/src/views/auth/Register.tsx
+++ dashboard/src/views/auth/Register.tsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import { FaFacebook, FaGoogle } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 const Register = () => {
     return (
-        <div>Register</div>
+        <div className='min-w-screen min-h-screen bg-[#ecebff] flex justify-center items-center overflow-hidden'>
+            <div className="md:w-[500px] sm:[350px] bg-blue-500 text-white sm:2 md:p-4">
+                <h2 className="text-xl mb-2 font-bold">Welcome to Ecommerce</h2>
+                <p className="text-sm mb-3 font-medium">Please register your account</p>
+
+                <form>
+                    <div className='flex flex-col w-full gap-1 mb-3'>
+                        <label htmlFor="name">Name</label>
+                        <input className='px-3 py-2 outline-none border border-blue-400 bg-transparent rounded-md' type="text" name='name' placeholder='Name' id='name' required />
+
+                    </div>
+
+
+                    <div className='flex flex-col w-full gap-1 mb-3'>
+                        <label htmlFor="email">Email</label>
+                        <input className='px-3 py-2 outline-none border border-blue-400 bg-transparent rounded-md' type="email" name='email' placeholder='Email' id='email' required />
+
+                    </div>
+
+                    <div className='flex flex-col w-full gap-1 mb-3'>
+                        <label htmlFor="password">Password</label>
+                        <input className='px-3 py-2 outline-none border border-blue-400 bg-transparent rounded-md' type="password" name='password' placeholder='Password' id='password' required />
+                    </div>
+
+                    <div className='flex items-center w-full gap-3 mb-3'>
+                        <input className='w-4 h-4 text-blue-600 overflow-hidden border border-blue-400 rounded focus:ring-amber-50' type="checkbox" name="checkbox" id="checkbox" />
+                        <label htmlFor="checkbox"> I agree to privacy policy & treams</label>
+                    </div>
+
+                    <button className='bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Sign Up</button>
+
+                    <div className='flex items-center mb-3 gap-3 justify-center'>
+                        <p>Already Have an account ? <Link className='font-bold' to="/login">Sing In</Link> </p>
+                    </div>
+
+                    <div className='w-full flex justify-center items-center mb-3'>
+                        <div className='w-[45%] bg-amber-50 h-px'></div>
+                        <div className='w-[10%] flex justify-center items-center'>
+                            <span className='pb-1'>Or</span>
+                        </div>
+                        <div className='w-[45%] bg-amber-50 h-px'></div>
+                    </div>
+
+                    <div className='flex justify-center items-center gap-3'>
+                        <div className='w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+                            <span><FaGoogle /></span>
+                        </div>
+
+                        <div className='w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg hover:shadow-blue-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+                            <span><FaFacebook /></span>
+                        </div>
+
+                    </div>
+
+
+                </form>
+            </div>
+        </div>
     )
 }
 

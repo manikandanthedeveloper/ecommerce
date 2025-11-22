87adbaeb11682831359a6f4ad9f835dc2fda0cd0
Manikandan Dhanapal <manikandanthedeveloper@gmail.com>
2025-11-22 17:07:24 +0530

    Dashboard Order page ui

--- dashboard/src/router/AdminRoutes.tsx
+++ dashboard/src/router/AdminRoutes.tsx
@@ -1,11 +1,23 @@
 import { lazy } from "react";
+import CategoryPage from "../views/admin/CategoryPage";
 
 const AdminDashboard = lazy(() => import("../views/admin/AdminDashboard"));
+const OrdersPage = lazy(() => import("../views/admin/OrdersPage"))
 
 export const adminRoutes = [
     {
         path: "admin/dashboard",
         element: <AdminDashboard />,
+        role: "admin",
+    },
+    {
+        path: "admin/dashboard/orders",
+        element: <OrdersPage />,
+        role: "admin",
+    },
+    {
+        path: "admin/dashboard/categories",
+        element: <CategoryPage />,
         role: "admin",
     },
 ];
